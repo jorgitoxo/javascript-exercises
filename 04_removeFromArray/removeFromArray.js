53ca3e0d5131a7c8958e74f1269f0926e61ae067
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-11-27 00:32:17 +1300

    Remove args from numbers
    Run through each elements in args
    Make sure that each one is removed from numbers

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(numbers, ...args) {
+    for (let i = 0; i < args.length; i++) {
+        const takeout = args[i];
+        while (numbers.includes(takeout)) {
+            numbers.splice(numbers.indexOf(takeout), 1);
+        }
+    }
+    return numbers;
 };
 
 // Do not edit below this line
