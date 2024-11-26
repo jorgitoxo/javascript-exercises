5a0bc96c21bc70df54ae95f7f28cc31947f12d1b
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-11-26 22:26:10 +1300

    Complete reverseString function
    Made a function to flip words only
    Then call it on each word of the phrase given
    Then add each reversed word at the start of the array
    Return the array with flipped words in reverse order
    All tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,27 @@
-const reverseString = function() {
+const reverseString = function(phrase) {
+    let splitWords = phrase.split(" ");
+    let reversedArray = [];
+    let reversedPhrase = "";
 
+    function flipString (string) {
+        let flippedArray = [];
+        let flippedString = "";
+
+        for (let i = 0; i < string.length; i++) {
+            flippedArray.unshift(string.charAt(i));
+        }
+        flippedString = flippedArray.join("");
+        return flippedString;
+    }
+
+    for (const word of splitWords) {
+        const flippedWord = flipString(word);
+        reversedArray.unshift(flippedWord);
+    }
+
+    reversedPhrase = reversedArray.join(" ");
+    
+    return reversedPhrase;
 };
 
 // Do not edit below this line
