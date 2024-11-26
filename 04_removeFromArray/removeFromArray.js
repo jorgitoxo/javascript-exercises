db916f8159ee7d1c82b448309776b5c8710ed48e
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-11-27 00:41:26 +1300

    Change for loop
    Used .forEach() method instead to iterate through args

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,9 @@
 const removeFromArray = function(numbers, ...args) {
-    for (let i = 0; i < args.length; i++) {
-        const takeout = args[i];
-        while (numbers.includes(takeout)) {
-            numbers.splice(numbers.indexOf(takeout), 1);
+    args.forEach(element => {
+        while (numbers.includes(element)) {
+            numbers.splice(numbers.indexOf(element), 1);
         }
-    }
+    });
     return numbers;
 };
 
