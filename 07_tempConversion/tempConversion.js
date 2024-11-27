e53a15a3a432e2a58379c3cd2e44a45ea69772ef
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-11-27 20:45:45 +1300

    Code refactor
    Removed redundant code

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,10 @@
 const convertToCelsius = function(temp) {
-  let convertedTemp = 0;
-  convertedTemp = (parseInt(temp) - 32) * (5/9);
-
+  let convertedTemp = (temp - 32) * (5/9);
   return convertedTemp ? parseFloat(convertedTemp.toFixed(1)) : convertedTemp;
 };
 
 const convertToFahrenheit = function(temp) {
-  let convertedTemp = 0;
-  convertedTemp = (temp * 9/5 + 32);
+  let convertedTemp = (temp * 9/5 + 32);
   return convertedTemp ? parseFloat(convertedTemp.toFixed(1)) : convertedTemp;
 };
 
