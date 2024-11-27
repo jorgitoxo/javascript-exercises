7ac5add7275fbca8b50e42f4e0d467f50b5d575d
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-11-27 19:24:21 +1300

    Decide if year provided is a leap year
    Used if statements for readability
    If year is divisible by 100, so too must it be divisible by 400
    Otherwise, it must be divisible by 4

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
+const leapYears = function(year) {  
+    // Broken down solution using if
+    // Crucial to evaluate division by 100 before division by 400
+    if (year%100 === 0) {
+        if (year%400 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        if (year%4 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    };
 
-};
+    // Ternary operator solution [fixed]
+    // return  (year%100) ?
+    //         ((year%4) ? false : true)
+    //         : ((year%400) ? false : true);
+}
 
 // Do not edit below this line
 module.exports = leapYears;
