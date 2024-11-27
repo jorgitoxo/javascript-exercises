54530848a9852289d10663be1d4c1271fbe17d8d
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-11-27 20:34:56 +1300

    Convert Celsius and Fahrenheit temperatures
    toFixed returns a string value, casted with parseFloat

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let convertedTemp = 0;
+  convertedTemp = (parseInt(temp) - 32) * (5/9);
+
+  return convertedTemp ? parseFloat(convertedTemp.toFixed(1)) : convertedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let convertedTemp = 0;
+  convertedTemp = (temp * 9/5 + 32);
+  return convertedTemp ? parseFloat(convertedTemp.toFixed(1)) : convertedTemp;
 };
 
 // Do not edit below this line
