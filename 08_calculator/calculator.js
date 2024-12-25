cba2047baacc20491b74720566a44a33f562a846
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-12-25 21:20:42 +1300

    Completed functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => total * num);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let numArray = [];
+  for (let i = 0; i < num; i++) {
+    numArray[i] = i + 1;
+  }
+  return numArray.reduceRight((total, num) => total * num, 1);
 };
 
 // Do not edit below this line
