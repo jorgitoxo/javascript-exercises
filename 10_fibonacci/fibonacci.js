4c8acaaa8fcfeb37b896128e6a12f0d113bb6b4f
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-12-26 20:26:26 +1300

    Generate nth number in fibonacci sequence
    Used object to keep a buffer of last two preceding numbers
    Updated buffer only after we are passed the 0th fibonacci number
    Hence our 0th and 1st elements in the sequence is 1

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fiboBuffer = {n1:0, n2: 1};
+    let fiboNum = 0;
+    const fiboPos = parseInt(num);
 
+    for (let i = 0; i < fiboPos; i++) {
+        fiboNum = fiboBuffer.n1 + fiboBuffer.n2
+        if (i >= 1) {
+            fiboBuffer.n1 = fiboBuffer.n2;
+            fiboBuffer.n2 = fiboNum;
+        }
+    }
+
+    return (fiboPos >= 0) ? fiboNum : "OOPS";
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+// console.log(fibonacci(0));
