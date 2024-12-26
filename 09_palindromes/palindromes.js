92bc7f8d1771060ec9d7b7f8578c86a75e7e06d8
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-12-26 18:35:08 +1300

    Check for palindrome
    Used a regex to filter the original string off non
    numerical and digit characters
    Reversed it and checked for equality

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // compare string char at with item at index in reversed array
+    // [\W_] : Matches any non-word character (equivalent to [^a-zA-Z0-9_])
+    /* [aA-zZ0-9] : Matches a single character in the range between a (index 97) and z (index 122) (case sensitive)
+     /d or [0-9] Matches a digit (equivalent to [0-9]) */
+    strippedStr = str.toLowerCase().split("").filter(char => /[aA-zZ0-9]/g.test(char)).join("");
+    reversedStr = str.toLowerCase().split("").filter(char => /[aA-zZ0-9]/g.test(char)).reverse().join("");
 
+    return (strippedStr === reversedStr);
 };
 
 // Do not edit below this line
