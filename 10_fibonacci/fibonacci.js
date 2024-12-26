621bbde362264b8627628cdb2236f7ea94304ab8
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-12-26 21:20:53 +1300

    Code refactor
    Removed IF check from inside FOR loop
    To avoid entering IF statement n-1 times
    We now enter IF statement once at most

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -3,18 +3,18 @@ const fibonacci = function(num) {
     let fiboNum = 0;
     const fiboPos = parseInt(num);
 
-    for (let i = 0; i < fiboPos; i++) {
-        fiboNum = fiboBuffer.n1 + fiboBuffer.n2
-        if (i >= 1) {
-            fiboBuffer.n1 = fiboBuffer.n2;
-            fiboBuffer.n2 = fiboNum;
-        }
+    if (fiboPos === 0)
+        return fiboBuffer.n1;
+    if (fiboPos === 1)
+        return fiboBuffer.n2;
+    
+    for (let i = 2; i <= fiboPos; i++) {
+        fiboNum = fiboBuffer.n1 + fiboBuffer.n2;
+        fiboBuffer.n1 = fiboBuffer.n2;
+        fiboBuffer.n2 = fiboNum;
     }
-
     return (fiboPos >= 0) ? fiboNum : "OOPS";
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
-
-// console.log(fibonacci(0));
